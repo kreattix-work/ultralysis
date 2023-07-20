7b4b54c21937a99319cf8c3b0d4906d94707abcb
Prince Patel <prpatel0949@gmail.com>
2023-07-20 23:52:51 +0530

    docs for manage-servers

--- includes/footer.php
+++ includes/footer.php
@@ -5,9 +5,9 @@
 
 <script src="https://unpkg.com/ag-grid-community/dist/ag-grid-community.min.js"></script>
 
-<script src="<?= BASEURL.'node_modules/jquery/dist/jquery.min.js' ?>"></script>
-<script src="<?= BASEURL.'node_modules/@popperjs/core/dist/umd/popper.min.js' ?>"></script>
-<script src="<?= BASEURL.'node_modules/bootstrap/dist/js/bootstrap.min.js' ?>"></script>
-<script src="<?= BASEURL.'node_modules/select2/dist/js/select2.full.min.js' ?>"></script>
+<script src="<?= ASSETS_URL.'/js/jquery.min.js' ?>"></script>
+<script src="<?= ASSETS_URL.'/js/popper.min.js' ?>"></script>
+<script src="<?= ASSETS_URL.'/js/bootstrap.min.js' ?>"></script>
+<script src="<?= ASSETS_URL.'/js/select2.full.min.js' ?>"></script>
 
 <script src="<?= ASSETS_URL.'/js/scripts.js?'.time() ?>"></script>
