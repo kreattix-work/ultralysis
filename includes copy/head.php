8df6da893943e61808e68b812dc109096ee1654c
Prince Patel <prpatel0949@gmail.com>
2023-07-22 17:38:34 +0530

    commit

--- includes copy/head.php
+++
@@ -1,5 +0,0 @@
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Ultralysis</title>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700&display=swap">
-<link rel="stylesheet" href="../assets/css/ultrasysis.min.css?<?= time() ?>">
